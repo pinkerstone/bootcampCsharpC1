99e89db86fe1d721cb44751d82fa257bf7ab9e01
Luis Ulloa <luisulloa@hotmail.com>
2024-09-04 20:30:28 -0500

    Version 1

--- Sesion3/Clase1Base/Program.cs
+++ Sesion3/Clase1Base/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Dynamic;
+using System.Net.Mail;
+
 Console.WriteLine("Hello, World!");
+int edad = 20;
+long altura = 181;
+string nombre = "Gonzalo";
+double dinero = 1651.36;
+float danio = 124.4f;
+decimal montoBanco = 1245151.4m;
+char letra = 'A';
+bool tieneGorro = false;
+
+Console.WriteLine("Mi nombre es {0}", nombre);
+Console.WriteLine($"Mi altura es {altura}");
+
+
+Console.WriteLine("Escribe tu nombre");
+nombre = Console.ReadLine();
+Console.WriteLine($"Mi nombre es {nombre}");
+
+Console.WriteLine("Escribe tu dinero");
+dinero = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Mi dinero es {dinero}");
