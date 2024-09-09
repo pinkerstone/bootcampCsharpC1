2102b9558e91d1fc581bface829d06b78d3df309
Luis Gabriel Ulloa <luisulloa@hotmail.com>
2024-09-09 00:11:06 -0500

    Objetos

---
+++ Sesion4/POO/POO/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Guerrero
+    {
+        public string nombre;
+        public int nivel = 0;
+        public double experiencia = 0;
+        public bool tieneOrejas;
+        public int orejasDeVictimas;
+        public float ataque = 35f;
+
+        public Guerrero()
+        {
+
+        }
+        public Guerrero(string argNombre)
+        {
+            this.nombre = argNombre;
+        }
+        public void Atacar()
+        {
+            Console.WriteLine("Atacando");
+        }
+        
+        public int VerNive()
+        {
+            return nivel;
+        }
+
+        public void RecibirExperiencia (double nuevaExperiencia)
+        {
+            experiencia += nuevaExperiencia;
+            if (experiencia > 10)
+            {
+                nivel++;
+                experiencia = 0;
+            }
+        }
+    }
+}
---
+++ Sesion4/POO/POO/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POO;
+
+Guerrero nuevoGuerrero = new Guerrero();
+
+nuevoGuerrero.nombre = "Pedrito";
+
+Console.WriteLine($"El nombre del guerrero es {nuevoGuerrero.nombre}");
+nuevoGuerrero.Atacar();
+Console.WriteLine(nuevoGuerrero.nivel);
+Console.WriteLine($"El nivel del guerro es {nuevoGuerrero.experiencia}");
+nuevoGuerrero.RecibirExperiencia(5);
+Console.WriteLine(nuevoGuerrero.nivel);
+Console.WriteLine(nuevoGuerrero.experiencia);
+
+Guerrero Guerrero2 = new Guerrero("Jose");
+Console.WriteLine(Guerrero2.nombre);
