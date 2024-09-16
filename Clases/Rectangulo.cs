072796b82ffe2f81ef8c907fb60a2dba8a378734
Luis Gabriel Ulloa <luisulloa@hotmail.com>
2024-09-16 16:38:47 -0500

    Estructuras y clases

---
+++ Clases/Rectangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Rectangulo
+    {
+        public int base1;
+        public int altura;
+
+        public int CalcularArea()
+        {
+            return base1 * altura;
+        }
+
+        public int CalcularPerimetro()
+        {
+            return (base1 + altura) * 2;
+        }
+    }
+}
---
+++ Estructuras/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public struct Persona
+{
+    public string nombre;
+    public string ciudad;
+    public int edad;
+}
+
+public class Producto
+{
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Persona persona = new Persona();
+        persona.edad = 36;
+        persona.nombre = "Hugo";
+        persona.ciudad = "Nasca";
+
+    }
+}
