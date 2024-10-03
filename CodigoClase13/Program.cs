79c7145d9317fd77a7e0989f23dfe93f1242a25d
Luis Gabriel Ulloa <luisulloa@hotmail.com>
2024-10-02 21:14:57 -0500

    Excepciones basicas

--- CodigoClase13/Program.cs
+++ CodigoClase13/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+int[] arr = new int[] { 2, 6, 7, 8, 3 };
+
+try
+{
+    Console.WriteLine("Ingresa un indice a mostrar");
+    int indice = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(arr[indice]);
+}
+catch (IndexOutOfRangeException ex)
+{
+    Console.WriteLine("Error en el indice");
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Error de formato");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error del programa");
+}
+
+
 
